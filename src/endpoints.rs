839e258e53fe7e9276056bc51cdc7031e509b15e
Jacob Birkett <jacob@birkett.dev>
2022-06-22 20:31:08 -0700

    Revise comments in endpoint! macro

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -13,6 +13,14 @@ use crate::Error;
 
 pub static DEFAULT_API_BASE: &str = "https://api.modrinth.com/v2/";
 
+/// This macro makes use of several calls to [`Result::unwrap`] or
+/// [`Option::unwrap`]. The values that are unwrapped are expected to be of
+/// types where the operation in question is guaranteed to be successful.
+/// It may be the case that an unwrap fails at runtime; if the author making use
+/// of the macro is certain that the hard-coded values are correct, but runtime
+/// panics and unwinds, this is considered a bug. A panic means that a variant
+/// needs to be added to the `Error` type, and that one of the following
+/// justification comments is wrong.
 macro_rules! endpoint {
     (
         $client:ident $method:ident,
@@ -26,20 +34,20 @@ macro_rules! endpoint {
         #[allow(unused_mut)]
         let mut uri = endpoint!(@uri, $base, $path $(, [$($var),*])?);
         // Use of unwrap:
-        // As with the request body and the URI, the value being serialized as
-        // query parameters shouldn't realistically fail because it is expected
-        // to be strongly-typed, with fields also of types that implement
-        // `Serialize` and are strongly-typed themselves.
+        // The type of `$params` is expected to have been validated manually,
+        // with a guarantee that it can be serialized as a query string with
+        // [`serde_qs::to_string`]. This would only fail if runtime values fail
+        // to serialize; this won't happen if the type of `$params` has a
+        // well-defined structure.
         $(uri.set_query(Some(&serde_qs::to_string($params).unwrap()));)?
 
         let builder = isahc::Request::builder()
             .method(endpoint!(@str $method))
             .uri(uri.as_str());
         // Use of unwrap:
-        // The request is built by this macro, and there should be enough
-        // structured information here that building request (when the body is
-        // added in another branch of code) is guaranteed to succeed.
-        // Mistakes here *should* have been caught by a compile-time error.
+        // Building the [`isahc::Request`] should realistically never fail,
+        // because all of the involved values have already made it past every
+        // preceeding point where the runtime had the opprotunity to panic.
         let request = endpoint!(@build, builder $(, $body)?).unwrap();
 
         // Sending the request can easily fail, so this would get bubbled to
@@ -49,12 +57,15 @@ macro_rules! endpoint {
         let mut bytes = Vec::new();
 
         // Use of unwrap:
-        // I expect reading the bytes from a response body to be infallible.
-        // Responses must always return some data, it can't just be headers,
-        // so unwrapping the result of the `read_to_end` here should be
-        // perfectly safe.
+        // Expect that reading the bytes from a response body is infallible.
+        // Responses must always return some data, even an empty slice of bytes,
+        // so unwrapping the result of the [`AsyncReadExt::read_to_end`] here
+        // should be perfectly acceptable.
         response.into_body().read_to_end(&mut bytes).await.unwrap();
 
+        // If the response status is not 200 OK, bubble the error, passing along
+        // the unexpected status, the fully formed URI, and the body bytes in
+        // case the server responded with more details.
         if status != 200 {
             return Err(Error::StatusNotOk { uri, status, bytes: Box::new(bytes) });
         }
@@ -62,6 +73,9 @@ macro_rules! endpoint {
         let deser = &mut serde_json::Deserializer::from_slice(bytes.as_slice());
         let result = serde_path_to_error::deserialize(deser);
 
+        // Determine if the response's body bytes deserialized correctly into
+        // the inferred type (outside the macro), and if not, bubble the error
+        // to `Error::Deserialize`.
         match result {
             Ok(value) => Ok(ApiResponse { bytes, value }),
             Err(error) => Err(Error::Deserialize { uri, error, bytes: Box::new(bytes) }),
@@ -69,29 +83,29 @@ macro_rules! endpoint {
     }};
     (@uri, $base:ident, $path:literal) => {
         // Use of unwrap:
-        // The `$base` is most likely hard-coded, or at the very least expected
-        // to be validated ahead-of-time. The `$path` is definitely hard-coded,
-        // and the user of the crate is responsible for ensuring its
-        // correctness.
+        // This cannot fail as a result of a malformed `$base`, which is most
+        // likely hard-coded, and at the very least, a parsing failure would
+        // have already been caught. The `$path` is definitely hard-coded in
+        // this branch with no variables. If this fails, the macro input was not
+        // correct.
         $base.join($path).unwrap()
     };
     (@uri, $base:ident, $path:literal, [$($var:expr),+]) => {
         // Use of unwrap:
-        // Formatting `$path` with `$var` items realistically shouldn't fail as
-        // long as the type-system is utilized. Types used as `$var` should have
-        // a consistent `Display` implimentation, and no matter the value of the
-        // type, should serialize into something that can both be sent over the
-        // internet and understood by the recipient.
-        $base.join(&format!($path, $($var),*)).unwrap()
+        // The call to [`url::Url::join`] takes a string that is produced by
+        // `format!`, where parts of `$path` are replaced, in order, by `$var`
+        // items with `ToString`. If it fails, the macro input was not correct.
+        $base.join(&format!($path, $($var.to_string()),*)).unwrap()
     };
     (@build, $builder:ident) => {
         $builder.body(())
     };
     (@build, $builder:ident, $body:expr) => {
         // Use of unwrap:
-        // Serializing the request body as JSON should not fail as long as it is
-        // strongly-typed, with fields implementing `Serialize`, which is
-        // expected here.
+        // The type of `$body` is expected to be validated manually. The user of
+        // this macro should be confident that the type will serialize
+        // successfully as a valid query string, even if the parameters of are
+        // variadic at runtime.
         $builder.body(serde_json::to_string($body).unwrap())
     };
     (@str GET) => {
