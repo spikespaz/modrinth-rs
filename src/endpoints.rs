7166248c5ed0b0250cfbf0b6da7aaf813cf20ca0
Jacob Birkett <jacob@birkett.dev>
2022-06-22 21:50:21 -0700

    Move error variants into own types

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -9,9 +9,39 @@ use crate::types::project::Project;
 use crate::types::search::ProjectSearchResult;
 use crate::types::version::{FileHashes, ProjectVersion};
 use crate::utils::new_struct;
-use crate::Error;
 
 pub static DEFAULT_API_BASE: &str = "https://api.modrinth.com/v2/";
+
+/// This is the most useful variant. This will be returned if the API
+/// response failed to parse either as valid JSON, or according to the
+/// policy for handling unknown fields set by the enabled Cargo features.
+/// See the crate documentation for [conditional
+/// compilation](crate#conditional-compilation).
+#[derive(Debug, thiserror::Error)]
+#[error("failed to deserialize a response from:\n{uri}\n{error}")]
+pub struct DeserializeError {
+    /// The URI that the initial request was sent to.
+    pub uri: url::Url,
+    /// The source error that this variant was constructed from.
+    #[source]
+    pub error: serde_path_to_error::Error<serde_json::Error>,
+    /// The body content bytes of the response.
+    pub bytes: Vec<u8>,
+}
+
+/// A request to a URI that was expected to return successfully with 200
+/// OK has failed to do so. This contains the status code that was received
+/// instead, and the bytes in the body of the response.
+#[derive(Debug, thiserror::Error)]
+#[error("received unsuccessful status code {status} from:\n{uri}")]
+pub struct ResponseError {
+    /// The URI that the initial request was sent to.
+    pub uri: url::Url,
+    /// The response status code that was received, not 200 OK.
+    pub status: isahc::http::StatusCode,
+    /// The body content bytes of the response.
+    pub bytes: Vec<u8>,
+}
 
 /// This macro makes use of several calls to [`Result::unwrap`] or
 /// [`Option::unwrap`]. The values that are unwrapped are expected to be of
@@ -67,7 +97,7 @@ macro_rules! endpoint {
         // the unexpected status, the fully formed URI, and the body bytes in
         // case the server responded with more details.
         if status != 200 {
-            return Err(Error::StatusNotOk { uri, status, bytes });
+            return Err(ResponseError { uri, status, bytes }.into());
         }
 
         let deser = &mut serde_json::Deserializer::from_slice(bytes.as_slice());
@@ -78,7 +108,7 @@ macro_rules! endpoint {
         // to `Error::Deserialize`.
         match result {
             Ok(value) => Ok(ApiResponse { bytes, value }),
-            Err(error) => Err(Error::Deserialize { uri, error, bytes }),
+            Err(error) => Err(DeserializeError { uri, error, bytes }.into()) ,
         }
     }};
     (@uri, $base:ident, $path:literal) => {
--- src/lib.rs
+++ src/lib.rs
@@ -6,44 +6,23 @@ pub mod types;
 pub(crate) mod serde_with;
 pub(crate) mod utils;
 
+use endpoints::{DeserializeError, ResponseError};
+
 // pub mod prelude {}
 
 /// The main error type used throughout the crate.
 #[non_exhaustive]
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    /// This is the most useful variant. This will be returned if the API
-    /// response failed to parse either as valid JSON, or according to the
-    /// policy for handling unknown fields set by the enabled Cargo features.
-    /// See the crate documentation for [conditional
-    /// compilation](crate#conditional-compilation).
-    #[error("failed to deserialize a response from:\n{uri}\n{error}")]
-    Deserialize {
-        /// The URI that the initial request was sent to.
-        uri: url::Url,
-        /// The source error that this variant was constructed from.
-        #[source]
-        error: serde_path_to_error::Error<serde_json::Error>,
-        /// The bytes the body content bytes of the response.
-        bytes: Vec<u8>,
-    },
+    #[error("{0}")]
+    Deserialize(#[from] DeserializeError),
     /// Sometimes the backend can throw an error, either because something was
-    /// configured wrongly or an internal error such as connection loss could
-    /// have happened.
+    /// configured wrongly or because of an internal error such as connection
+    /// loss.
     #[error("failed to construct a request or recieve a response\n{0}")]
     Request(#[from] isahc::Error),
-    /// A request to a URI that was expected to return successfully with `200:
-    /// OK` has failed to do so. This contains the status code that was recieved
-    /// instead, and the bytes in the body of the response.
-    #[error("recieved status {status} from:\n{uri}")]
-    StatusNotOk {
-        /// The URI that the initial request was sent to.
-        uri: url::Url,
-        /// The response status code that was returned, not 200 OK.
-        status: isahc::http::StatusCode,
-        /// The bytes the body content bytes of the response.
-        bytes: Vec<u8>,
-    },
+    #[error("{0}")]
+    Response(#[from] ResponseError),
     /*
     /// This variant will wrap an [`isahc::http::Error`] when configuring the
     /// client has failed to produce a stable instance of the backend.
